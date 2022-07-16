7c19997980670179f7a818a3d4b52e5cc132e286
timickb <timickb@yanex.ru>
2022-07-16 13:56:28 +0500

    dev: improved server tests

--- internal/app/server/server_internal_test.go
+++ internal/app/server/server_internal_test.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"errors"
 	"fmt"
 	"github.com/stretchr/testify/assert"
 	"net/http"
@@ -9,9 +10,42 @@ import (
 	"testing"
 )
 
-func TestServerRestoreEndpoint(t *testing.T) {
-	t.Helper()
+func TestDefaultConfig(t *testing.T) {
+	conf := DefaultConfig()
 
+	assert.Equal(t, conf.ServerPort, 8080)
+	assert.Equal(t, conf.MaxUrlLength, 300)
+	assert.Equal(t, conf.StoreImpl, "db")
+}
+
+func TestServerGeneralResponse(t *testing.T) {
+	rec := httptest.NewRecorder()
+	http.NewRequest(http.MethodGet, "/some-strange-endpoint", nil)
+
+	s, err := NewServer(&Config{ServerPort: 8080, StoreImpl: "test"})
+	if err != nil {
+		t.Fatalf("an error occured while creating server instance")
+	}
+
+	s.makeResponse(rec, 201, nil)
+	assert.Equal(t, rec.Code, http.StatusCreated)
+}
+
+func TestServerErrorResponse(t *testing.T) {
+	rec := httptest.NewRecorder()
+	http.NewRequest(http.MethodGet, "/some-strange-endpoint", nil)
+
+	s, err := NewServer(&Config{ServerPort: 8080, StoreImpl: "test"})
+	if err != nil {
+		t.Fatalf("an error occured while creating server instance")
+	}
+
+	s.error(rec, 400, errors.New("bad request"))
+	assert.Equal(t, rec.Code, http.StatusBadRequest)
+
+}
+
+func TestServerRestoreEndpoint(t *testing.T) {
 	rec := httptest.NewRecorder()
 	req, err := http.NewRequest(http.MethodGet, "/restore?hash=t3st_h4sh_", nil)
 
@@ -30,8 +64,6 @@ func TestServerRestoreEndpoint(t *testing.T) {
 }
 
 func TestServerCreateEndpoint(t *testing.T) {
-	t.Helper()
-
 	payloadString := `{"url": "https://example.com"}`
 
 	rec := httptest.NewRecorder()
@@ -49,4 +81,44 @@ func TestServerCreateEndpoint(t *testing.T) {
 	s.ServeHTTP(rec, req)
 
 	assert.Equal(t, rec.Code, http.StatusOK)
+}
+
+func TestServerCreateEndpointInvalidJSON(t *testing.T) {
+	payloadString := `invalid json`
+
+	rec := httptest.NewRecorder()
+	req, err := http.NewRequest(http.MethodPost, "/create", strings.NewReader(payloadString))
+
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+	}
+
+	s, err := NewServer(&Config{ServerPort: 8080, StoreImpl: "test"})
+	if err != nil {
+		t.Fatalf("an error occured while creating server instance")
+	}
+	s.ServeHTTP(rec, req)
+
+	assert.NotEqual(t, rec.Code, http.StatusOK)
+}
+
+func TestServerCreateEmptyURL(t *testing.T) {
+	payloadString := `{"url": ""}`
+
+	rec := httptest.NewRecorder()
+	req, err := http.NewRequest(http.MethodPost, "/create", strings.NewReader(payloadString))
+
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+	}
+
+	s, err := NewServer(&Config{ServerPort: 8080, StoreImpl: "test"})
+	if err != nil {
+		t.Fatalf("an error occured while creating server instance")
+	}
+	s.ServeHTTP(rec, req)
+
+	assert.Equal(t, rec.Code, http.StatusBadRequest)
 }
